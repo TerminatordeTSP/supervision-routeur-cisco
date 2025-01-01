25a5cfa775db1b6c05efad61a552d6bb3a7719cb
TerminatordeTSP <ryan.zerhouni@telecom-sudpairs.eu>
2025-01-01 23:23:10 +0100

    Implémentation initiale de la page de configuration d'un routeur (partie ajout des seuils)

---
+++ src/config_routeur/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.core.validators import MinValueValidator
+
+class Seuils(models.Model):
+    ram = models.FloatField(
+        validators=[MinValueValidator(0.1)],  # minimum 0.1
+        verbose_name="RAM en Mo",
+    )
+    CPU = models.FloatField(
+        validators=[MinValueValidator(1)],  # minimum 1
+        verbose_name="CPU utilisé en %",
+    )
+    trafic = models.FloatField(
+        validators=[MinValueValidator(0.001)],
+        verbose_name="Bande passante en Mo/s",
+    )

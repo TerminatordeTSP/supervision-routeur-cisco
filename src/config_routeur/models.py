227cc2d23c6cd746805327b4e3c39796af468983
TerminatordeTSP <ryan.zerhouni@telecom-sudpairs.eu>
2025-01-03 01:49:54 +0100

    ajout des champs  : "ram, CPU, trafic, nom" a la classe seuil ainsi que définition des verbose_name pour chacun de ces champs. Dans la bdd sqlite j'ai ajouté un seui pour tester depuis le site admin. Dans admin.py j'ai indiqué que je voulais en plus d'avoir accès a la classe seuil sur le site admin, afficher sur celui ci la ram, CPU, trafic et nom de chacun des seuils créée.

--- src/config_routeur/models.py
+++ src/config_routeur/models.py
@@ -15,3 +15,11 @@ class Seuil(models.Model):
         validators=[MinValueValidator(0.001)],
         verbose_name="Bande passante en Mo/s",
     )
+    nom = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name="Nom du seuil"
+    )
+
+
+

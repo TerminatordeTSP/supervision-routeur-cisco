eccc6f467c4b679c737b499de885f33902075c51
TerminatordeTSP <ryan.zerhouni@telecom-sudpairs.eu>
2025-01-04 00:24:24 +0100

    La page de configuration des routeurs permet désormais d'afficher les informations sur les configuration actuelle des routeurs et des seuils. De plus lorsque qu'on clique sur un routeur ou un seuil ca redirige vers une page qui donne des détails sur lui.

--- src/config_routeur/views.py
+++ src/config_routeur/views.py
@@ -9,3 +9,15 @@ def configuration(request):
     routeurs = Routeur.objects.all()
     seuils = Seuil.objects.all()
     return render(request, 'Page de configuration.html',{'routeurs': routeurs,'seuils':seuils})
+
+def configuration_routeur_detail(request, id):  # notez le paramètre id supplémentaire
+   routeur = Routeur.objects.get(id=id)
+   return render(request,
+          'routeur_detail.html',
+                 {'routeur': routeur}) # nous passons l'id au modèle
+
+def configuration_seuil_detail(request, id):  # notez le paramètre id supplémentaire
+   seuil = Seuil.objects.get(id=id)
+   return render(request,
+          'seuil_detail.html',
+                 {'seuil': seuil}) # nous passons l'id au modèle

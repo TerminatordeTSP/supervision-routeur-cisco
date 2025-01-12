c1dfe7cdd77abc446ca669fe535ffc488a8795cd
Othmane <othmane.tairech@gmail.com>
2025-01-12 01:09:41 +0100

    creation des options dans /settings qui dirigent vers de nouvelles vues

--- src/settings_app/urls.py
+++ src/settings_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, user_info, appearance, language
 
 urlpatterns = [
-    path('settings/', index, name="settings_index"),
+    path('', index, name="settings_index"),
+    path('user_info/', user_info, name="user_info"),
+    path('appearance/', appearance, name="appearance"),
+    path('language/', language, name="language")
 ]

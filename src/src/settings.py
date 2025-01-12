4b16253b46e2b301bfa4f0ed2a4f14b35cbfeac4
Paul Lardet <paullardet@tutanota.com>
2025-01-12 18:17:03 +0100

    chore: influxdb & new apps setup

--- src/src/settings.py
+++ src/src/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INFLUXDB_SETTINGS = {
+    'url': 'http://172.16.10.40:8086',
+    'token': 'gSTSmKrP5RImMoYLS2w4fTFfi3WOCzRRP63R_AcVYnzAsqnA6uOo1i0ec63jG0wd3tX6qpC00g7b0D0k34RBfg==',
+    'org': 'TSP',
+    # 'bucket': 'your-bucket'
+}
--- src/src/urls.py
+++ src/src/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from alerts.views import show_alerts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('alerts/', show_alerts, name='show_alerts'),
 ]

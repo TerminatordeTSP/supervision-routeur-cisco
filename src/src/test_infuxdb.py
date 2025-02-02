1b38d1a2620f997556371f5fd54910e31b86217b
Paul Lardet <paul@Pauls-iMac.local>
2025-02-02 22:14:57 +0100

    chore(cpu): get cpu info

--- src/src/test_infuxdb.py
+++ src/src/test_infuxdb.py
@@ -1,4 +1,4 @@
-import influxdb_client, os, time
+import os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -9,9 +9,18 @@ bucket = "cisco-supervisor"
 
 client = InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
-
+   
+"""for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="TSP", record=point)
+  time.sleep(1) # separate points by 1 second
 """
-for value in range(5):
+
+"""for value in range(5):
     point = (
         Point("measurement1")
         .tag("tagname1", "tagvalue1")
@@ -20,10 +29,10 @@ for value in range(5):
     write_api.write(bucket=bucket, org="TSP", record=point)
     time.sleep(1) # separate points by 1 second
 
-print("Data written successfully.")
-"""
+print("Data written successfully.")"""
 
-'''
+
+
 query_api = client.query_api()
 
 query = """from(bucket: "cisco-supervisor")
@@ -34,15 +43,3 @@ tables = query_api.query(query, org="TSP")
 for table in tables:
   for record in table.records:
     print(f'{record}\n')
-'''
-
-query_api = client.query_api()
-
-query = """from(bucket: "cisco-supervisor")
- |> range(start: -10m)
- |> filter(fn: (r) => r._measurement == "measurement1")"""
-tables = query_api.query(query, org="TSP")
-
-for table in tables:
-  for record in table.records:
-    print(record)

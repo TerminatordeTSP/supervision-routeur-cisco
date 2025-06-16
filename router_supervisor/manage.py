9dfab8eba591c0055a4adbe82c2185f89af2e9bd
TerminatordeTSP <ryan.zerhouni@telecom-sudpairs.eu>
2025-06-16 11:22:48 +0200

    changement

--- router_supervisor/manage.py
+++ router_supervisor/manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # Add the parent directory to sys.path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def main():

5096e65e0e08990b1c352fc97a380c409328b37c
Paul Lardet <paul@Pauls-iMac.local>
2025-06-19 22:27:43 +0200

    chore: remove old reference commit

--- supervision-routeur-cisco-eef6b3b9c2afef65424456de614fa1b422742903/router_supervisor/dashboard_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import index
-
-urlpatterns = [
-    path('', index, name="dashboard_index"),
-    # path('dashboard/', include("dashboard_app.urls")),
-]

a8b0f71a6374446fe203f32c06f0ca1ec40e1547
TerminatordeTSP <ryan.zerhouni@telecom-sudpairs.eu>
2025-07-13 22:33:47 +0200

    Je viens d'implémenter "mots de passe oublié", tout marche maintenant coté authentification.

--- router_supervisor/src/urls.py
+++ router_supervisor/src/urls.py
@@ -5,8 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 import sys, os
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
-
+from django.urls import path
+from router_supervisor.settings_app.views import CustomPasswordResetView
+from django.contrib.auth import views as auth_views
 # Import des gestionnaires d'erreur personnalisés
 from . import error_views
 
@@ -61,6 +64,34 @@ urlpatterns = [
     path('reset/done/',
      auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
      name='password_reset_complete'),
+
+path(
+        'password-reset/',
+        CustomPasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete')
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
